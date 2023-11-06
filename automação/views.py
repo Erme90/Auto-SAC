79b48e1f74e8deae0eed3b28d90b9c7bf9df8b29
Emerson Freire <79375451+Erme90@users.noreply.github.com>
2023-11-06 16:52:55 -0300

    Configuração do envio de email automatico

--- automação/views.py
+++ automação/views.py
@@ -16,6 +16,34 @@ from selenium.common.exceptions import NoSuchElementException, WebDriverExceptio
 from time import sleep
 import os
 from dotenv import load_dotenv
+import smtplib
+import email.message
+
+
+#Função de envio do email automatico com as instruções para o usuário.
+def enviar_email(email_usuario):  
+    corpo_email = """
+    <p>Teste de email_usuario.</p>
+    <p>Usuário criado com sucesso !</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "Teste de email_usuario"
+    msg['From'] = 'emersonnascimento.freire@gmail.com'
+    msg['To'] = email_usuario
+    password = 'puzb mlvc salm wzns'
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('email_usuario enviado')
+
+    enviar_email()
+
 
 load_dotenv(override=True)
 
@@ -33,8 +61,10 @@ class Formulario(forms.Form):
     usuario_desejado = forms.CharField(max_length=100, required=True, error_messages={'required': 'Campo obrigatório.'})
     nome_completo = forms.CharField(max_length=100, required=True, error_messages={'required': 'Campo obrigatório.'})
     matricula = forms.CharField(max_length=10, required=True, error_messages={'required': 'Campo obrigatório.'})
-    email = forms.EmailField(required=True, error_messages={'required': 'Campo obrigatório.'})
+    email_usuario = forms.EmailField(required=True, error_messages={'required': 'Campo obrigatório.'})
     vinculo = forms.ChoiceField(choices=[('', 'Selecione um vínculo'),('Funcamp', 'Funcamp'), ('Unicamp', 'Unicamp')], required=True, error_messages={'required': 'Selecione um vinculo.'})
+
+
 
 
 #Esta função, será chamada pela função "cria_usuario", para iniciar o processo de liberação e envio da senha ao usuário.
@@ -62,7 +92,7 @@ class Formulario(forms.Form):
     input()
     navegador.find_element('xpath', '/html/body/form/table[2]/tbody/tr/td/table/tbody/tr/td[3]/input').click'''
 
-    #email(email) #chama a função de envio de email.
+    #email_usuario(email_usuario) #chama a função de envio de email_usuario.
 
 #Esta função faz a execução do selenium para criação da senha nova (primeira senha) do SISE.
 def cria_usuario(request):
@@ -74,9 +104,9 @@ def cria_usuario(request):
             usuario_desejado = form.cleaned_data['usuario_desejado']   
             nome_completo = form.cleaned_data['nome_completo']  
             matricula = form.cleaned_data['matricula']
-            email = form.cleaned_data['email']
+            email_usuario = form.cleaned_data['email_usuario']
             vinculo = form.cleaned_data['vinculo']
-            print('O usuário desejado é:', usuario_desejado, nome_completo,matricula, email, vinculo)
+            print('O usuário desejado é:', usuario_desejado, nome_completo,matricula, email_usuario, vinculo)
        
             try:                
                 navegador.get('https://www1.sistemas.unicamp.br/SiSeCorp/publico/solicitacao_username/formsolicitacaousername.do')   #site que será automatizado.
@@ -90,7 +120,7 @@ def cria_usuario(request):
                     navegador.find_element('xpath', '//*[@id="SolicitacaoUsernameActionForm"]/div/div[1]/div/div/div[2]/div[1]/select/option[2]').click()
                     navegador.find_element('xpath', '//*[@id="div_matricula_unicamp"]/input').send_keys(matricula)
                 sleep(1)
-                navegador.find_element('xpath', '//*[@id="SolicitacaoUsernameActionForm"]/div/div[1]/div/div/div[2]/input[2]').send_keys(email)#preenche o campo 'email'
+                navegador.find_element('xpath', '//*[@id="SolicitacaoUsernameActionForm"]/div/div[1]/div/div/div[2]/input[2]').send_keys(email_usuario)#preenche o campo 'email_usuario'
                 navegador.find_element('xpath', '//*[@id="SolicitacaoUsernameActionForm"]/div/div[1]/div/div/div[3]/input[1]').click()#Clica no botão de envio do formulário
                 navegador.find_element('xpath', '//*[@id="ConfirmarSolicitacaoUsernameActionForm"]/div/div[1]/div/div/div[3]/input[1]').click()#confirma a criação do usuário.
                
@@ -104,12 +134,16 @@ def cria_usuario(request):
                 mensagem_erro = f'ERRO: Verifique os dados informados e tente novamente'
                 form.add_error(None, mensagem_erro)
                 print('Agora vai renderizar a página, com uma msg de erro!')
+                
+                
                 return render(request, 'index.html', {'form': form, 'mensagem_erro': mensagem_erro})#redireciona para a mesma página, porém com o aviso de "Erro"
             finally:
+                enviar_email(email_usuario)
+                print('enviou o email')
                 navegador.quit()
         else:
             form = Formulario() 
-        mensagem_sucesso = f'Usuário criado com sucesso! Verifique o e-mail {email} para instruções.'
+        mensagem_sucesso = f'Usuário criado com sucesso! Verifique o e-mail {email_usuario} para instruções.'
         msg = {
             'msg_sucesso': mensagem_sucesso
         }
@@ -117,30 +151,5 @@ def cria_usuario(request):
         return render(request, 'index.html', msg)
     
 
-#EMAIL AUTOMATICO
-def email(email):
-    resultado_tarefa = "teste concluído com sucesso"
-    detalhes = "A tarefa automatizada foi concluída sem problemas."
+#email_usuario AUTOMATICO
 
-
-    #variaveis com os dados do email
-    
-    assunto= 'Teste do projeto SAC'
-    texto_email = f'Resultado: {resultado_tarefa}\nDetalhes: {detalhes}'
-    remetente = 'emersonnascimento.freire@gmail.com' #este email enviará o email para o teste
-    destinatario = email #este email foi preenchido pelo usuário no form
-    print('pegou os dados do email')
-    
-    #Configuração do servidor para envio do email:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend', print('EMAIL_BACKEND')
-    EMAIL_HOST = 'smtp.gmail.com', print('EMAIL_HOST')
-    EMAIL_PORT = 587, print('EMAIL_PORT')
-    EMAIL_USE_TLS = True, print('EMAIL_USE_TLS')
-    EMAIL_HOST_USER = 'emersonnascimento.freire@gmail.com', print('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = '[e]91046344', print('EMAIL_HOST_PASSWORD')
-
-    #utilização da função 'send_mail'
-    print('Vai chamar a função "send_mail"')
-
-    send_mail(assunto, texto_email, remetente, [destinatario], fail_silently=False)
-    print('email enviado')
